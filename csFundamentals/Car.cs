95a32aac16bc54018c0b087decb70bc647a58c7e
Rodney Chery <rchery7@wgu.edu>
2024-12-25 09:55:26 -0500

    Objects Practice

---
+++ csFundamentals/Car.cs
@@ -0,0 +1,23 @@
+﻿namespace csFundamentals;
+
+class Car
+{
+    String make;
+    String model;
+    int year;
+    String color;
+
+    public Car(String make, String model, int year, String color)
+    {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.color = color;
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine("You drive the " + color + " " + make + " " + model);
+    }
+      
+}
--- csFundamentals/Program.cs
+++ csFundamentals/Program.cs
@@ -12,40 +12,19 @@ namespace csFundamentals  // object = an instance of a class
     {
         static void Main(string[] args)
         {
-            Human human1 = new Human();
-            Human human2 = new Human();
+            Car car1 = new Car("Chevy", "Camaro", 2020, "Black");
+            Car car2 = new Car("Toyota", "Prius", 1999, "Silver");
+            Car car3 = new Car("Honda", "Civic", 2024, "Blue");
             
-            
-           human1.name = "Rick"; 
-           human1.age = 20;
-           
-           human2.name = "Morty";
-           human2.age = 16;
-           
-           human1.Eat();
-           human1.Sleep();
+         car1.Drive();
+         car2.Drive();
+         car3.Drive();
 
-           Console.ReadKey();
+         Console.ReadKey();
         }
     }
 
-    class Human
-    {
-        public string name;
-        public int age;
-
-        public void Eat()
-        {
-            Console.WriteLine("I'm eating {}");
-            
-            
-        }
-
-        public void Sleep()
-        {
-            Console.WriteLine("I'm sleeping");
-        }
-    }
+    
     
 }
 
