c3f317b4939e51da9cfa65318cde769628790edd
Rodney Chery <rchery7@wgu.edu>
2024-12-25 11:00:02 -0500

    C# Inheritance

--- csFundamentals/Program.cs
+++ csFundamentals/Program.cs
@@ -4,21 +4,54 @@ using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
 
-namespace csFundamentals  // overloaded constaructors = technique to create multiple constructors,
-                            // with different set of parameters.
-                            // name + parameters = signature
+namespace csFundamentals  // inheritance = 1 or more child classes recieving fields, methods, etc. from a common parent class 
 {
-    public class Program
+    public class Vehicle
+    {
+        public int speed = 0;
+
+        public void go()
+        {
+            Console.WriteLine("The vehicle is going...");
+        }
+    }
+    
+    public class Car : Vehicle
+    {
+        public int wheels = 4;
+    }
+
+    public class Bicycle : Vehicle
+    {
+        public int wheels = 2;
+    }
+
+    public class Boat : Vehicle
+    {
+        public int wheels = 4;
+    }
+
+    public class Program  // entry point 
     {
         static void Main(string[] args)
         {
-            Pizza pizza1 = new Pizza("white","tomato","yellow", "Sausage");
-            Pizza pizza2 = new Pizza("wheat", "bechamel", "gruyere");
+
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+            Boat boat = new Boat();
             
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
             
-            
-            
-            Console.ReadKey();
+            Console.WriteLine(bicycle.speed);
+            Console.WriteLine(bicycle.wheels);
+            car.go();
+                
+            Console.WriteLine(boat.speed);
+            Console.WriteLine(boat.wheels);
+            car.go();
+
         }
     }
 
--- csFundamentals/obj/Debug/net9.0/csFundamentals.AssemblyInfo.cs
+++ csFundamentals/obj/Debug/net9.0/csFundamentals.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csFundamentals")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010c0bbb7a4fcb70d600b23139d65155c1d58cc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+519733daca29e09fd7c11e5030eae7f2f7d1f7b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("csFundamentals")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csFundamentals")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
